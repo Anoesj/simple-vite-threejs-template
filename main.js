77dcc76424e9938341a726627ed81eb6591f99ef
Anoesj <anoesjsadraee@gmail.com>
2022-01-07 12:02:04 +0100

    Clean up

--- main.js
+++ main.js
@@ -54,17 +54,12 @@ scene.add(pointLightHelper);
 const ambientLight = new THREE.AmbientLight(0xfffdf7, 0.70);
 scene.add(ambientLight);
 
-// const ambientLightHelper = new THREE.AmbientLightHelper(ambientLight, 5);
-// scene.add(ambientLightHelper);
-
 
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 15; // zoom out
-camera.position.x = 4;
-camera.position.y = 4;
+camera.position.set(4, 4, 15); // zoom out
 scene.add(camera);
 
 
@@ -76,7 +71,6 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.physicallyCorrectLights = true;
 const render = () => { renderer.render(scene, camera); };
 
 addWindowResizeCallback(() => {
